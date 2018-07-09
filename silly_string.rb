91dd0952a23a4fadc586fa4b114fac539acd8d81
Ella Zhang <eyanchizhang@gmail.com>
2018-07-09 20:03:39 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,9 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = "cat"
+your_favorite_word = "Cat"
 
-puts "cat #{your_favorite_word}"
+puts "Cat #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
