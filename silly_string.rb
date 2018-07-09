7f69d141905e80a0d5f075741c5937cc1f6a315f
Ella Zhang <eyanchizhang@gmail.com>
2018-07-09 20:05:06 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -13,9 +13,9 @@ puts "CAT #{favorite_word_in_all_caps}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "I'M NOT SHOUTING"
+phrase = "I'M NOT SHOUTING" 
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase.swapcase # Put a . after phrase and use a String method.
 
 puts lowercased_phrase
 
